59d1d046573029d2229edc51f0f398b748824ff6
astek <jeremy.brunel@bashroom.com>
2024-11-11 23:05:56 +0100

    feat: better mario mouvements , thx : https://www.youtube.com/watch?v=JOytyzK7Z94

--- scripts/player.gd
+++ scripts/player.gd
@@ -24,8 +24,8 @@ enum Sounds {
 
 @export_group("Locomotion")
 @export var run_speed_damping = 0.5
-@export var speed = 200.0
-@export var jump_velocity = -350
+@export var speed = 75.0
+@export var jump_velocity = -400
 @export_group("")
 
 @export_group("Stomping enemies")
@@ -36,46 +36,101 @@ enum Sounds {
 
 var player_mode = PlayerMode.SMALL
 
+# wye
 
+@export var max_walk_speed: float = 75.0
+@export var max_run_speed: float = 135.0
+@export var max_sprint_speed: float = 135.0
+
+@export var walk_accel: float = 337.5
+@export var stop_decel: float = 225
+
+@export var p_meter_starting_speed: float = 131.25
+@export var max_p_meter: float = 1.867
+
+var p_meter: float = 0.0
+var facing_dir = -1
+# wye end
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	handle_jump()
-	
-	handle_direction_x(delta)
-
-	move_and_slide()
-
-
-func handle_jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 		play_sound(Sounds.JUMP)
-
-	# Manage short jump if jump btn released	
+		
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= 0.5
 
-func handle_direction_x(delta):
 	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = lerp(velocity.x, speed * direction, run_speed_damping * delta)
+	if is_on_floor() and abs(velocity.x) >= p_meter_starting_speed and Input.is_action_pressed("shoot"):
+		p_meter = min(p_meter + 2 * delta, max_p_meter)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed * delta)
+		p_meter = max(p_meter - delta, 0)
 		
+	if direction:
+		var max_speed: float = max_walk_speed
+		if Input.is_action_pressed("shoot"):
+			max_speed = max_run_speed
+		if p_meter == max_p_meter:
+			max_speed = max_sprint_speed
+		velocity.x = move_toward(velocity.x, direction * max_speed, walk_accel * delta)
+		facing_dir = direction
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, stop_decel * delta)
+		
+#	if velocity.x:
+#		if p_meter == 1.867:
+#			# animation walk
+#		else:
+#			#animation run
+	
+	
 	animation.trigger_animation(velocity, direction, player_mode)
+	
+	move_and_slide()
 
+#
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y += gravity * delta
+#
+#	handle_jump()
+#
+#	handle_direction_x(delta)
+#
+#	move_and_slide()
+#
+#
+#func handle_jump():
+#	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		velocity.y = jump_velocity
+#		play_sound(Sounds.JUMP)
+#
+#	# Manage short jump if jump btn released	
+#	if Input.is_action_just_released("jump") and velocity.y < 0:
+#		velocity.y *= 0.5
+#
+#func handle_direction_x(delta):
+#	var direction = Input.get_axis("left", "right")
+#	if direction:
+#		velocity.x = lerp(velocity.x, speed * direction, run_speed_damping * delta)
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, speed * delta)
 
+	
+#
+#
 func play_sound(sound):
 	if sound == Sounds.JUMP && jump_sound.playing == false:
 		jump_sound.play()
 	if sound == Sounds.STOMP && stomp_sound.playing == false:	
 		stomp_sound.play()
-
-
+#
+#
 func _on_area_2d_area_entered(area):
 	if area is Enemy:
 		handle_enemy_collision(area)
@@ -84,7 +139,7 @@ func handle_enemy_collision(enemy: Enemy):
 	if enemy == null:
 		printerr("player collided with null enemy")
 		return
-	
+
 	var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
 	if angle_of_collision > min_stomp_degree && maxx_stomp_degree > angle_of_collision:
 		enemy.die()
--- scripts/player_animated_sprite.gd
+++ scripts/player_animated_sprite.gd
@@ -5,6 +5,11 @@ func trigger_animation(velocity: Vector2, direction: int, player_mode: Player.Pl
 	var animation_prefix = Player.PlayerMode.keys()[player_mode].to_snake_case()
 	
 	if not get_parent().is_on_floor():
+		#TODO: jump / fall
+		#if velocity.y > 0
+			# fall
+		# else
+			#jump
 		play("%s_jump" % animation_prefix)
 	
 	# handle slide animation	
@@ -18,6 +23,8 @@ func trigger_animation(velocity: Vector2, direction: int, player_mode: Player.Pl
 			scale.x = -1
 		elif scale.x == -1 && sign(velocity.x) == 1:
 			scale.x = 1
+			
+			
 	
 	# handle run and idle animations
 	if velocity.x != 0:
