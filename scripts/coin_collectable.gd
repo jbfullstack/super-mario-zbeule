55857522c1ffd523875f212d08a6a05a757ba7d7
astek <jeremy.brunel@bashroom.com>
2024-11-13 09:15:04 +0100

    feat: yoshi coin

--- scripts/coin_collectable.gd
+++ scripts/coin_collectable.gd
@@ -1,23 +1,22 @@
 extends Area2D
 class_name CoinCollectable
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
-	
-
+@onready var animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
 
 func _on_body_entered(body):
 	if body is Player:
 		set_collision_layer_value(6, false)
 		set_collision_mask_value(1, false)
-	
-		var spawn_tween = get_tree().create_tween()
-	
+		
 		GlobalAudioPlayer.play_sound(GlobalAudioPlayer.Sounds.COIN)
-		
-		animated_sprite_2d.play("default", 10, false)
-		
-		spawn_tween.tween_property(self, "position", position + Vector2(0, -10), 0.4)
-		spawn_tween.tween_callback(queue_free)
+	
+		play_collect_animation()
 		
 		get_tree().get_first_node_in_group("level_manager").on_coin_collected()
 		
+
+func play_collect_animation():
+	var spawn_tween = get_tree().create_tween()
+	animated_sprite_2d.play("default", 10, false)
+	spawn_tween.tween_property(self, "position", position + Vector2(0, -10), 0.4)
+	spawn_tween.tween_callback(queue_free)
---
+++ scripts/yoshi_coin.gd
@@ -0,0 +1,16 @@
+extends CoinCollectable
+class_name YoshiCoin
+
+
+func play_collect_animation():
+	var spawn_tween = get_tree().create_tween()
+#	animated_sprite_2d.play("default", 10, false)
+#	spawn_tween.tween_property(self, "position", position + Vector2(0, -10), 0.4)
+#	spawn_tween.tween_callback(queue_free)
+
+	var tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_ELASTIC)
+#	tween.tween_property(self, "position", position + Vector2(0, -10), 0.1)
+	tween.tween_property(self, "modulate", Color.RED, 0.1)
+	tween.tween_property(self, "scale", Vector2(), 1)
+	tween.tween_callback(queue_free)
+
